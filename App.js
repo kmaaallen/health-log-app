3352bc37786924a34de45b6713850562cb63f510
kmaaallen@gmail.com <kmaaallen@gmail.com>
2021-07-10 12:21:33 +0100

    feat: Add navigation to log page screen

--- App.js
+++ App.js
@@ -1,13 +1,20 @@
-import { StatusBar } from 'expo-status-bar';
 import React from 'react';
 import { StyleSheet, Text, View } from 'react-native';
+//Navigation
+import { NavigationContainer } from '@react-navigation/native';
+import { createStackNavigator } from '@react-navigation/stack';
+//Screens
+import LogPage from './src/screens/LogPage';
+
+const Stack = createStackNavigator();
 
 export default function App() {
   return (
-    <View style={styles.container}>
-      <Text>Open up App.js to start working on your app!</Text>
-      <StatusBar style="auto" />
-    </View>
+    <NavigationContainer>
+      <Stack.Navigator>
+        <Stack.Screen name="Log" component={LogPage} />
+      </Stack.Navigator>
+    </NavigationContainer>
   );
 }
 
