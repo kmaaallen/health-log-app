fc5ffabab93c0e2917bb6df3b9bd4ecad0b8f0ea
kmaaallen@gmail.com <kmaaallen@gmail.com>
2021-07-10 12:20:41 +0100

    feat: Add log page screen to app

---
+++ src/screens/LogPage.js
@@ -0,0 +1,13 @@
+import React from 'react';
+import { Text, View } from 'react-native';
+
+
+function LogPage() {
+    return (
+        <View style={{ flex: 1, alignItems: 'center', justifyContent: 'center' }}>
+            <Text>Log Page</Text>
+        </View>
+    );
+}
+
+export default LogPage;
