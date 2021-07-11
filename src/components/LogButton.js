b2c91388e56173217b7d3baac9bb26c71f2147ca
kmaaallen@gmail.com <kmaaallen@gmail.com>
2021-07-11 22:08:37 +0100

    feat: Change to class component with redux

--- src/components/LogButton.js
+++ src/components/LogButton.js
@@ -1,34 +1,30 @@
-import React, { useState, useLayoutEffect, useRef } from 'react';
+import React, { Component } from 'react';
 import { Card, Paragraph } from 'react-native-paper';
+//REDUX
+import { connect } from 'react-redux';
+import { incrementCount } from '../redux/actions';
 
-
-const LogButton = () => {
-    const [count, setCount] = useState(0);
-    const [updated, setUpdated] = useState('never');
-
-    const firstUpdate = useRef(true);
-
-    useLayoutEffect(() => {
-        if (firstUpdate.current) {
-            firstUpdate.current = false;
-            return;
-        } else {
-            var now = Date.now();
-            var date = new Date(now);
-            var dateTime = date.toLocaleString();
-            setUpdated(dateTime);
-        }
-    }, [count])
-
-    return (
-        <Card onPress={() => { setCount(count + 1) }} style={{ height: 200, width: 200 }}>
-            <Card.Title title="My Habit" />
-            <Card.Content>
-                <Paragraph>{count}</Paragraph>
-                <Paragraph>Last logged: {updated}</Paragraph>
-            </Card.Content>
-        </Card>
-    );
+class LogButton extends Component {
+    render() {
+        return (
+            <Card onPress={this.props.incrementCount} style={{ height: 200, width: 200 }}>
+                <Card.Title title="My Habit" />
+                <Card.Content>
+                    <Paragraph>{this.props.count}</Paragraph>
+                    <Paragraph>Last logged: {this.props.updated}</Paragraph>
+                </Card.Content>
+            </Card>
+        );
+    }
 };
 
-export default LogButton;
+const mapStateToProps = (state) => ({
+    count: state.countReducer.count,
+    updated: state.countReducer.updated
+})
+
+const mapActionsToProps = {
+    incrementCount,
+}
+
+export default connect(mapStateToProps, mapActionsToProps)(LogButton);
