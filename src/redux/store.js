9cb4d93c9db4359c179780a8e950f336298257e4
kmaaallen@gmail.com <kmaaallen@gmail.com>
2021-07-11 22:07:09 +0100

    feat: Add redux persist to store

--- src/redux/store.js
+++ src/redux/store.js
@@ -1,7 +1,27 @@
 import { createStore, combineReducers } from 'redux';
+import AsyncStorage from '@react-native-async-storage/async-storage'
+import { persistStore, persistReducer } from 'redux-persist';
 
-//import xx from './reducers';
+import countReducer from './reducers';
 
-const rootReducer = combineReducers({});
+const rootReducer = combineReducers({ countReducer: countReducer });
 
-export const store = createStore(rootReducer);
+//Redux Persist Config
+const persistConfig = {
+    key: 'root',
+    storage: AsyncStorage,
+    whitelist: [
+        'countReducer',
+    ],
+};
+
+const persistedReducer = persistReducer(persistConfig, rootReducer);
+
+const store = createStore(persistedReducer);
+
+let persistor = persistStore(store);
+
+export {
+    store,
+    persistor,
+};
