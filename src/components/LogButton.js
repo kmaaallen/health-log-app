a02788ffdb053cd7624a05d2e8492e833d79ecf0
kmaaallen@gmail.com <kmaaallen@gmail.com>
2021-07-21 08:53:30 +0100

    style: Add basic styling to button for readability

--- src/components/LogButton.js
+++ src/components/LogButton.js
@@ -1,17 +1,35 @@
 import React, { Component } from 'react';
-import { Button, Paragraph } from 'react-native-paper';
+import { Button, Paragraph, Title } from 'react-native-paper';
+import { StyleSheet } from 'react-native';
 //REDUX
 import { connect } from 'react-redux';
 import { incrementCount } from '../redux/actions';
 import { hasReachedDailyLimitSelector } from '../redux/selectors';
 
+const styles = StyleSheet.create({
+    habitButton: {
+        padding: 20,
+    },
+    habitButtonText: {
+        display: 'block',
+        textAlign: 'center',
+        fontSize: '15px',
+        padding: 10
+    },
+    habitCount: {
+        display: 'block',
+        textAlign: 'center',
+        fontSize: '30px'
+    }
+});
+
 export class LogButton extends Component {
     render() {
         return (
-            <Button disabled={this.props.hasReachedLimit} onPress={this.props.incrementCount}>
-                <Paragraph>My Habit</Paragraph>
-                <Paragraph>{this.props.count}</Paragraph>
-                <Paragraph>Last logged:{this.props.updated}</Paragraph>
+            <Button style={styles.habitButton} disabled={this.props.hasReachedLimit} onPress={this.props.incrementCount}>
+                <Title style={styles.habitButtonText}>My Habit</Title>
+                <Paragraph style={styles.habitCount}>{this.props.count}</Paragraph>
+                <Paragraph style={styles.habitButtonText}>Last logged: {this.props.updated}</Paragraph>
             </Button>
         );
     }
