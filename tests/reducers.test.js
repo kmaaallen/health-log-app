bb58d9a00e29288335a68870ac6a038bb9ff0775
kmaaallen@gmail.com <kmaaallen@gmail.com>
2021-07-29 20:39:01 +0100

    test: Add tests set limit, reset count handling

--- tests/reducers.test.js
+++ tests/reducers.test.js
@@ -2,7 +2,7 @@ import countReducer from '../src/redux/reducers';
 
 describe('test countReducer', () => {
     it('should return 0 and null as initial state', () => {
-        expect(countReducer(undefined, {})).toEqual({ count: 0, updated: null });
+        expect(countReducer(undefined, {})).toEqual({ count: 0, updated: null, limit: 1 });
     });
     it('should handle INCREMENT_COUNT', () => {
         expect(
@@ -12,4 +12,19 @@ describe('test countReducer', () => {
             })
         ).toEqual({ count: 1, updated: 1612235045000 });
     });
+    it('should handle SET_LIMIT', () => {
+        expect(
+            countReducer({ count: 0, updated: null, limit: 1 }, {
+                type: 'SET_LIMIT',
+                payload: { limit: 3 }
+            })
+        ).toEqual({ count: 0, updated: null, limit: 3 });
+    });
+    it('should handle RESET_COUNT', () => {
+        expect(
+            countReducer({ count: 3 }, {
+                type: 'RESET_COUNT',
+            })
+        ).toEqual({ count: 0 });
+    })
 });
