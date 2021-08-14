a5970e76f8013cc1c9c163f4f44a078c1c9e6ed8
kmaaallen@gmail.com <kmaaallen@gmail.com>
2021-08-14 16:34:08 +0100

    Test: Update reducer tests for habits as objects

--- tests/reducers.test.js
+++ tests/reducers.test.js
@@ -7,103 +7,103 @@ describe('test countReducer', () => {
     it('should handle INCREMENT_COUNT', () => {
         expect(
             countReducer({
-                habits: [{
-                    id: '1',
-                    count: 0,
-                    limit: 1,
-                    log: []
-                },
-                {
-                    id: '2',
-                    count: 0,
-                    limit: 1,
-                    log: []
-                }]
+                habits: {
+                    1: {
+                        count: 0,
+                        limit: 1,
+                        log: []
+                    },
+                    2: {
+                        count: 0,
+                        limit: 1,
+                        log: []
+                    }
+                }
             }, {
                 type: 'INCREMENT_COUNT',
-                payload: { updated: new Date('02 Feb 2021 03:04:05 GMT').valueOf(), id: '1' }
+                payload: { updated: new Date('02 Feb 2021 03:04:05 GMT').valueOf(), id: 1 }
             })
         ).toEqual({
-            habits: [{
-                id: '1',
-                count: 1,
-                limit: 1,
-                log: [{ updated: 1612235045000, info: { type: 'increment' } }]
-            },
-            {
-                id: '2',
-                count: 0,
-                limit: 1,
-                log: []
-            }]
+            habits: {
+                1: {
+                    count: 1,
+                    limit: 1,
+                    log: [{ updated: 1612235045000, info: { type: 'increment' } }]
+                },
+                2: {
+                    count: 0,
+                    limit: 1,
+                    log: []
+                }
+            }
         });
     });
     it('should handle SET_LIMIT', () => {
         expect(
             countReducer({
-                habits: [{
-                    id: '1',
-                    count: 0,
-                    limit: 1,
-                    log: []
-                },
-                {
-                    id: '2',
-                    count: 0,
-                    limit: 1,
-                    log: []
-                }]
+                habits: {
+                    1: {
+                        count: 0,
+                        limit: 1,
+                        log: []
+                    },
+                    2: {
+                        count: 0,
+                        limit: 1,
+                        log: []
+                    }
+                }
             }, {
                 type: 'SET_LIMIT',
-                payload: { updated: new Date('02 Feb 2021 03:04:05 GMT').valueOf(), limit: 3, id: '1' }
+                payload: { updated: new Date('02 Feb 2021 03:04:05 GMT').valueOf(), limit: 3, id: 1 }
             })
         ).toEqual({
-            habits: [{
-                id: '1',
-                count: 0,
-                limit: 3,
-                log: [{ updated: 1612235045000, info: { type: 'limit' } }]
-            },
-            {
-                id: '2',
-                count: 0,
-                limit: 1,
-                log: []
-            }]
+            habits: {
+                1: {
+                    count: 0,
+                    limit: 3,
+                    log: [{ updated: 1612235045000, info: { type: 'limit' } }]
+                },
+                2: {
+                    count: 0,
+                    limit: 1,
+                    log: []
+                }
+            }
         });
     });
     it('should handle RESET_COUNT', () => {
         expect(
             countReducer({
-                habits: [{
-                    id: '1',
-                    count: 1,
+                habits: {
+                    1: {
+                        count: 1,
+                        limit: 1,
+                        log: []
+                    },
+                    2: {
+                        count: 0,
+                        limit: 1,
+                        log: []
+                    }
+                }
+            }, {
+                type: 'RESET_COUNT',
+                payload: { updated: new Date('02 Feb 2021 03:04:05 GMT').valueOf(), id: 1 }
+            })
+        ).toEqual({
+            habits: {
+                1: {
+                    count: 0,
                     limit: 1,
-                    log: []
+                    log: [{ updated: 1612235045000, info: { type: 'reset' } }]
                 },
-                {
-                    id: '2',
+                2: {
                     count: 0,
                     limit: 1,
                     log: []
-                }]
-            }, {
-                type: 'RESET_COUNT',
-                payload: { updated: new Date('02 Feb 2021 03:04:05 GMT').valueOf(), id: '1' }
-            })
-        ).toEqual({
-            habits: [{
-                id: '1',
-                count: 0,
-                limit: 1,
-                log: [{ updated: 1612235045000, info: { type: 'reset' } }]
-            },
-            {
-                id: '2',
-                count: 0,
-                limit: 1,
-                log: []
-            }]
+                }
+            }
         });
     })
 });
