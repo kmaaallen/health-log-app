f782280d3c76f6575c02c55f540ea7270ce72f0f
kmaaallen@gmail.com <kmaaallen@gmail.com>
2021-08-15 09:14:56 +0100

    Feat: Update LogPage to iterate over habits

--- src/screens/LogPage.js
+++ src/screens/LogPage.js
@@ -1,16 +1,26 @@
 import React from 'react';
 import { Text, View } from 'react-native';
 //components
-import LogButton from '../components/LogButton';
+import { LogButton } from '../components/LogButton';
+//REDUX
+import { connect } from 'react-redux';
 
 
-function LogPage() {
+function LogPage({ habits }) {
     return (
         <View style={{ flex: 1, alignItems: 'center', justifyContent: 'center' }}>
             <Text>Log Page</Text>
-            <LogButton />
+
+            {habits.length > 0 ? Object.keys({ habits }).map((habit) => {
+                return <LogButton id={habit.id} key={habit.id} />
+            }) : null}
+
         </View>
     );
 }
 
-export default LogPage;
+const mapStateToProps = (state) => ({
+    habits: state.count.habits
+})
+
+export default connect(mapStateToProps)(LogPage);
