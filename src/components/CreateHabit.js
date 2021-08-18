05b445257fa73fd5ccf6d7193ae0b1e7549e6a74
kmaaallen@gmail.com <kmaaallen@gmail.com>
2021-08-18 12:46:17 +0100

    Fix: Remove unused import

--- src/components/CreateHabit.js
+++ src/components/CreateHabit.js
@@ -1,5 +1,5 @@
 import React, { Component } from 'react';
-import { View, StyleSheet } from 'react-native';
+import { StyleSheet } from 'react-native';
 import { Button, Dialog, TextInput, Portal, Surface } from 'react-native-paper';
 //REDUX
 import { connect } from 'react-redux';
