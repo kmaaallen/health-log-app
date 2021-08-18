0e1afd7aaded6e6afa4dca5fec8afd06e9cb6c10
kmaaallen@gmail.com <kmaaallen@gmail.com>
2021-08-18 12:57:47 +0100

    Test: Add mock store and provider

--- tests/CreateHabit.test.js
+++ tests/CreateHabit.test.js
@@ -1,16 +1,28 @@
 import React from 'react';
+import { Provider } from 'react-redux';
 import renderer from 'react-test-renderer';
+import configureStore from 'redux-mock-store';
 
-import { CreateHabit } from '../src/components/CreateHabit';
+const mockStore = configureStore([]);
+
+import CreateHabit from '../src/components/CreateHabit';
 
 describe('<CreateHabit />', () => {
+
+    let store;
+
+    beforeEach(() => {
+        store = mockStore({ count: { habits: {} } });
+    });
+
+
     it('has 1 child', () => {
-        const tree = renderer.create(<CreateHabit />).toJSON();
+        const tree = renderer.create(<Provider store={store}><CreateHabit /></Provider>).toJSON();
         expect(tree.children.length).toBe(1);
     });
 
     it('renders correctly', () => {
-        const tree = renderer.create(<CreateHabit />).toJSON();
+        const tree = renderer.create(<Provider store={store}><CreateHabit /></Provider>).toJSON();
         expect(tree).toMatchSnapshot();
     })
 });
