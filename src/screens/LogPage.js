0d9fa6a06b75e754e04c46b6c47058d8a64c04f0
kmaaallen@gmail.com <kmaaallen@gmail.com>
2021-08-21 12:54:06 +0100

    Fix: Move provider to top level App

--- src/screens/LogPage.js
+++ src/screens/LogPage.js
@@ -1,25 +1,19 @@
 import React from 'react';
 import { ScrollView } from 'react-native';
-import { Provider, Portal } from 'react-native-paper';
 //components
 import LogButton from '../components/LogButton';
 import CreateHabit from '../components/CreateHabit';
 //REDUX
 import { connect } from 'react-redux';
 
-
 function LogPage({ habits }) {
     const habitButtons = habits ? Object.keys(habits) : [];
 
     return (
-        <Provider>
-            <Portal.Host>
-                <ScrollView>
-                    <CreateHabit />
-                    {habitButtons.map((item) => (<LogButton id={item} key={item} />))}
-                </ScrollView>
-            </Portal.Host>
-        </Provider>
+        <ScrollView>
+            <CreateHabit />
+            {habitButtons.map((item) => (<LogButton id={item} key={item} />))}
+        </ScrollView>
     );
 }
 
