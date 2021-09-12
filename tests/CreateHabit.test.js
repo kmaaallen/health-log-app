54b52f31dcfc9b85f599bacee866dd4733268d36
kmaaallen@gmail.com <kmaaallen@gmail.com>
2021-09-12 11:38:37 +0100

    Test: Update test to pass theme

--- tests/CreateHabit.test.js
+++ tests/CreateHabit.test.js
@@ -2,10 +2,12 @@ import React from 'react';
 import { Provider } from 'react-redux';
 import renderer from 'react-test-renderer';
 import configureStore from 'redux-mock-store';
+import { theme } from '../App';
 
 const mockStore = configureStore([]);
 
 import CreateHabit from '../src/components/CreateHabit';
+
 
 describe('<CreateHabit />', () => {
 
@@ -17,12 +19,12 @@ describe('<CreateHabit />', () => {
 
 
     it('has 1 child', () => {
-        const tree = renderer.create(<Provider store={store}><CreateHabit /></Provider>).toJSON();
+        const tree = renderer.create(<Provider store={store} ><CreateHabit theme={theme} /></Provider>).toJSON();
         expect(tree.children.length).toBe(1);
     });
 
     it('renders correctly', () => {
-        const tree = renderer.create(<Provider store={store}><CreateHabit /></Provider>).toJSON();
+        const tree = renderer.create(<Provider store={store} ><CreateHabit theme={theme} /></Provider>).toJSON();
         expect(tree).toMatchSnapshot();
     })
 });
