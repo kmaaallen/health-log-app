d08c8a74ba12be08f899fad648887eabd065926e
kmaaallen@gmail.com <kmaaallen@gmail.com>
2021-09-15 22:26:07 +0100

    Test: Update unit tests for createHabit component

--- tests/CreateHabit.test.js
+++ tests/CreateHabit.test.js
@@ -1,22 +1,20 @@
 import React from 'react';
 import { Provider } from 'react-redux';
-import renderer from 'react-test-renderer';
-import configureStore from 'redux-mock-store';
+import { Provider as PaperProvider } from 'react-native-paper';
 import { theme } from '../App';
-
-const mockStore = configureStore([]);
-
 import CreateHabit from '../src/components/CreateHabit';
-
+// Testing
+import '@testing-library/jest-dom/extend-expect';
+import { render, fireEvent, waitFor } from '@testing-library/react-native';
+import renderer from 'react-test-renderer';
+import { createTestStore } from './utils';
 
 describe('<CreateHabit />', () => {
-
     let store;
 
     beforeEach(() => {
-        store = mockStore({ count: { habits: {} } });
+        store = createTestStore();
     });
-
 
     it('has 1 child', () => {
         const tree = renderer.create(<Provider store={store} ><CreateHabit theme={theme} /></Provider>).toJSON();
@@ -26,5 +24,31 @@ describe('<CreateHabit />', () => {
     it('renders correctly', () => {
         const tree = renderer.create(<Provider store={store} ><CreateHabit theme={theme} /></Provider>).toJSON();
         expect(tree).toMatchSnapshot();
+    });
+
+    it('renders create new habit button, that when clicked shows dialog content and allows new habit to be created', async () => {
+        // Paper provider component wrapper required for react-native-paper, provided at app level
+        const { getByText, getByTestId, queryByText } = render(<Provider store={store}><PaperProvider theme={theme}><CreateHabit /></PaperProvider></Provider>);
+        const button = getByText('Create new habit');
+        // Check button is there
+        expect(button).toBeTruthy();
+        // Expect store to be empty at this stage
+        expect(store.getState().habits).toEqual({ "habits": {} });
+        // Click button
+        fireEvent.press(button);
+        await waitFor(() => queryByText('Create a habit'));
+        // Dialog with form present
+        const title = getByTestId('title-input');
+        const limit = getByTestId('limit-input');
+        const submit = getByText('Ok');
+        expect(title && limit).toBeTruthy();
+        // change form values and submit form
+        fireEvent.changeText(title, 'My first habit');
+        fireEvent.changeText(limit, '3');
+        fireEvent.press(submit);
+        // Expect store to be updated with one habit
+        expect(Object.keys(store.getState().habits).length).toBe(1);
+        expect(store.getState().habits.habits[1].title).toBe('My first habit');
+        expect(store.getState().habits.habits[1].limit).toBe('3');
     })
 });
